26bb30ae3fca5755280d830daa00df19e08cf067
pktolstov <pktolstov@gmail.com>
2025-08-02 23:17:15 +0300

    Improve middlewares logs

--- src/middlewares/loggerOne.js
+++ src/middlewares/loggerOne.js
@@ -1,5 +1,6 @@
 const loggerOne = (request, response, next) => {
   console.log(`📨 Запрос на: ${request.originalUrl}`);
+  console.log(`[${new Date().toISOString()}] ${request.method} ${request.originalUrl}`);
   next();
 };
 
